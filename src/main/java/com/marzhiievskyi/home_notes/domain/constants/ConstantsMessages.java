e4b2d97999a7d0a115923e3295d8f464cedafbc2
Dmytro Marzhiievskyi <freemanopposite@gmail.com>
2023-08-09 23:06:39 +0300

    in development

--- src/main/java/com/marzhiievskyi/home_notes/domain/constants/ConstantsMessages.java
+++ src/main/java/com/marzhiievskyi/home_notes/domain/constants/ConstantsMessages.java
@@ -3,8 +3,8 @@ package com.marzhiievskyi.home_notes.domain.constants;
 public class ConstantsMessages {
 
     //info
-    public final static String RELEASE = "RELEASE ";
-    public final static String VERSION = " 1.0.1 ";
+    public final static String RELEASE = "BETA ";
+    public final static String VERSION = " 1.0.1 ";//current version
     public final static String HOME_NOTE = "Home notes. Version ";
 
 }
