abf131097a045e6d909e22442cea1ffbb4022e32
Dmytro Marzhiievskyi <freemanopposite@gmail.com>
2023-08-09 19:52:42 +0300

    done: add: refactoring:

--- src/main/resources/static/data.sql
+++ src/main/resources/static/data.sql
@@ -1,6 +1,5 @@
-CREATE SCHEMA IF NOT EXISTS home_notes;
 
-CREATE TABLE home_notes.user
+CREATE TABLE IF NOT EXISTS user
 (
     id           BIGINT AUTO_INCREMENT,
     nickname     VARCHAR(15)  NOT NULL,
@@ -12,7 +11,7 @@ CREATE TABLE home_notes.user
     UNIQUE KEY `access_token` (`access_token`)
 ) COLLATE UTF8MB4_bin;
 
-CREATE TABLE home_notes.note
+CREATE TABLE IF NOT EXISTS note
 (
     id          BIGINT AUTO_INCREMENT,
     user_id     BIGINT NOT NULL,
@@ -22,7 +21,7 @@ CREATE TABLE home_notes.note
     FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE ON UPDATE CASCADE
 ) COLLATE UTF8MB4_bin;
 
-CREATE TABLE home_notes.tag
+CREATE TABLE IF NOT EXISTS tag
 (
     id          BIGINT AUTO_INCREMENT,
     text        VARCHAR(25) NOT NULL,
@@ -32,7 +31,7 @@ CREATE TABLE home_notes.tag
 ) COLLATE UTF8MB4_bin;
 INSERT INTO tag(text) VALUE ('mustBe=)');
 
-CREATE TABLE home_notes.note_tag
+CREATE TABLE IF NOT EXISTS note_tag
 (
     id          BIGINT AUTO_INCREMENT,
     note_id     BIGINT NOT NULL,
@@ -44,7 +43,7 @@ CREATE TABLE home_notes.note_tag
     UNIQUE `phrase_id_tag_id` (`note_id`, `tag_id`)
 ) COLLATE UTF8MB4_bin;
 
-CREATE TABLE home_notes.subscription
+CREATE TABLE IF NOT EXISTS subscription
 (
     id          BIGINT AUTO_INCREMENT,
     sub_user_id BIGINT    NOT NULL,
@@ -56,7 +55,7 @@ CREATE TABLE home_notes.subscription
     UNIQUE KEY sub_user_id_pub_user_id (sub_user_id, pub_user_id)
 ) COLLATE utf8_bin;
 
-CREATE TABLE home_notes.like_note
+CREATE TABLE IF NOT EXISTS like_note
 (
     id          BIGINT AUTO_INCREMENT,
     note_id     BIGINT    NOT NULL,
@@ -68,7 +67,7 @@ CREATE TABLE home_notes.like_note
     UNIQUE `phrase_id_user_id` (`note_id`, `user_id`)
 ) COLLATE utf8_bin;
 
-CREATE TABLE home_notes.comment
+CREATE TABLE IF NOT EXISTS comment
 (
     id          BIGINT AUTO_INCREMENT,
     user_id     BIGINT       NOT NULL,
@@ -80,7 +79,7 @@ CREATE TABLE home_notes.comment
     FOREIGN KEY (user_id) REFERENCES user (id)
 ) COLLATE utf8_bin;
 
-CREATE TABLE home_notes.block
+CREATE TABLE IF NOT EXISTS block
 (
     id              BIGINT AUTO_INCREMENT,
     user_id         BIGINT    NOT NULL,
